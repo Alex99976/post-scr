d4ee87338dfaa1b2e91db919bbc4d7e2c25a1cff
Alex99976 <alexandr99976@yandex.by>
2026-03-30 02:06:15 +0300

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -12,19 +12,21 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
 # =========================
 # BLOCK 1 — BASE TOOLS
 # =========================
-winget install --id 7zip.7zip -e --accept-package-agreements --accept-source-agreements
-winget install --id RARLab.WinRAR -e --accept-package-agreements --accept-source-agreements
-winget install --id Git.Git -e --accept-package-agreements --accept-source-agreements
-winget install --id OpenJS.NodeJS -e --accept-package-agreements --accept-source-agreements
-winget install --id Microsoft.DotNet.DesktopRuntime.10 -e --accept-package-agreements --accept-source-agreements
-winget install --id Notepad++.Notepad++ -e --accept-package-agreements --accept-source-agreements
+winget install --id 7zip.7zip -e --interactive
+winget install --id RARLab.WinRAR -e --interactive
+winget install --id Notepad++.Notepad++ -e --interactive
+winget install --id Git.Git -e --interactive
+winget install --id OpenJS.NodeJS -e --interactive
+winget install --id Microsoft.DotNet.DesktopRuntime.10 -e --interactive
 
 # =========================
 # BLOCK 2 — APPS
 # =========================
-winget install --id Discord.Discord -e
-winget install --id TunnelBear.TunnelBear -e
-winget install --id Nvidia.NvidiaApp -e
+winget install --id Discord.Discord -e --interactive
+winget install --id TunnelBear.TunnelBear -e --interactive
+winget install --id Nvidia.NvidiaApp -e --interactive
+winget install --id Valve.Steam -e --interactive
+winget install --id qBittorrent.qBittorrent -e --interactive
 
 # =========================
 # BLOCK 3 — MICROSOFT STORE
